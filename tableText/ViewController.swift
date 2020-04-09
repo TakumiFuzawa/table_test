a6bcab62ac4909b13c03974923213c4f05e3b871
github TakumiFuzawa <futaku0811@gmail.com>
2020-04-09 20:20:35 +0900

    Initial Commit

---
+++ tableText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableText
+//
+//  Created by Takumi Fuzawa on 2020/04/09.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
